5f5ee0a5bc0a0d9f86d9fbc116a108f413fc6ac5
Khushicodes15 <khushidhona06@gmail.com>
2025-07-23 20:52:21 +0530

    Find largest Number using C

---
+++ C/Largest/Largest.c
@@ -0,0 +1,11 @@
+class Solution {
+    public int largest(List<int> arr) {
+        int max = arr[0];  
+        for (int i = 1; i < arr.Count; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
